a6ef18ed13585821599b79b14bff4e31629abb83
qetzing <general@qetz.de>
2024-08-01 10:41:11 +0200

    Implement default creation of config file

--- config.go
+++ config.go
@@ -5,32 +5,75 @@ import (
 	"os"
 )
 
+type Config struct {
+	Mail      Mail      `yaml:"mail"`
+	PeopleApi PeopleApi `yaml:"peopleApi"`
+}
+
+type Mail struct {
+	Port     int    `yaml:"port"`
+	Host     string `yaml:"host"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Sender   string `yaml:"sender"`
+	Receiver string `yaml:"receiver"`
+	Tls      bool   `yaml:"tls"`
+	Secure   bool   `yaml:"secure"`
+}
+
+type PeopleApi struct {
+	AccessToken  string `yaml:"accessToken"`
+	TokenType    string `yaml:"tokenType"`
+	RefreshToken string `yaml:"refreshToken"`
+	Expiry       string `yaml:"expiry"`
+}
+
+const path = "config.yml"
+
 func ReadConfig(cfg *Config) error {
-	file, err := os.Open("config.yml")
+	file, err := os.Open(path)
+	if os.IsNotExist(err) {
+		if err := createDefaultConfig(); err != nil {
+			return err
+		}
+		file, err = os.Open(path)
+	}
 	if err != nil {
 		return err
 	}
-	//goland:noinspection GoUnhandledErrorResult
 	defer file.Close()
 
 	return yaml.NewDecoder(file).Decode(cfg)
 }
 
-type Config struct {
-	Mail struct {
-		Port     int    `yaml:"port"`
-		Host     string `yaml:"host"`
-		User     string `yaml:"user"`
-		Password string `yaml:"password"`
-		Sender   string `yaml:"sender"`
-		Receiver string `yaml:"receiver"`
-		Tls      bool   `yaml:"tls"`
-		Secure   bool   `yaml:"secure"`
-	} `yaml:"mail"`
-	PeopleApi struct {
-		AccessToken  string `yaml:"accessToken"`
-		TokenType    string `yaml:"tokenType"`
-		RefreshToken string `yaml:"refreshToken"`
-		Expiry       string `yaml:"expiry"`
-	} `yaml:"peopleApi"`
+var defaultConfig = Config{
+	Mail: Mail{
+		Port:     465,
+		Host:     "smpt.mail.com",
+		User:     "form",
+		Password: "HNJ38a",
+		Sender:   "form@mail.com",
+		Receiver: "admin@mail.com",
+		Tls:      false,
+		Secure:   false,
+	},
+	PeopleApi: PeopleApi{
+		AccessToken:  "",
+		TokenType:    "Bearer",
+		RefreshToken: "",
+		Expiry:       "2024-07-31T15:07:33.6502681+02:00",
+	},
+}
+
+func createDefaultConfig() error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := yaml.NewEncoder(file)
+	defer encoder.Close()
+
+	return encoder.Encode(defaultConfig)
 }
